bb874caee1dbff9370e748ec1899b57190b9103f
Nemanja Djokic <nemanja.djokic@vegait.rs>
2025-07-18 15:29:46 +0200

    setup db

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { db } from "@/lib";
+import { postsTable, usersTable } from "@/lib/schema";
 
-export default function Home() {
+export default async function Home() {
+
+  const post = await db.query.postsTable.findMany();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <form action={async () => {
+        'use server'
+        await db.insert(usersTable).values({
+          id: 1,
+          name: 'John Doe',
+          age: 30,
+          email: 'test@test.com'
+        })
+        await db.insert(postsTable).values({
+          title: 'New Post',
+          content: 'This is the content of the new post.',
+          userId: 1
+        })
+      }}>
+        <input type="text" name="name" placeholder="Name" required />
+        <button>Submit</button>
+      </form>
+      {
+        post.map((p) => (
+          <div key={p.id}>
+            <h2>{p.title}</h2>
+            <p>{p.content}</p>
+            <p>Author ID: {p.userId}</p>
+            <p>Created At: {new Date(p.createdAt).toLocaleString()}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
